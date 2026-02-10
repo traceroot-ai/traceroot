7c40741e57384cd7e5f0869cbdf2d99ab3c764b4
shirleyai-co <shirleyai615@gmail.com>
2026-02-09 18:48:06 -0800

    remove migration sql

--- frontend/packages/core/prisma/migrations/20260209234324_/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "workspaces_stripeCustomerId_idx" ON "workspaces"("stripeCustomerId");
-
--- CreateIndex
-CREATE INDEX "workspaces_stripeSubscriptionId_idx" ON "workspaces"("stripeSubscriptionId");
